b5b3fe7984353903f8ae5e8a9474c119cfe9d718
emily-erickson <105762424+emily-erickson@users.noreply.github.com>
2024-05-06 15:46:58 -0600

    Lab2-2.2

---
+++ Lab2_InterruptNesting/Src/main.c
@@ -0,0 +1,181 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2024 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+	SystemClock_Config();
+	__HAL_RCC_GPIOC_CLK_ENABLE();
+	__HAL_RCC_SYSCFG_CLK_ENABLE();
+	
+	// What happens when SysTick doesn't have the highest priority?
+	NVIC_SetPriority(SysTick_IRQn,2);
+
+  /* USER CODE BEGIN Init */
+	GPIO_InitTypeDef initLEDS = {GPIO_PIN_6 | GPIO_PIN_7 | GPIO_PIN_8 | GPIO_PIN_9,
+															GPIO_MODE_OUTPUT_PP,
+															GPIO_SPEED_FREQ_LOW,
+															GPIO_NOPULL};
+	GPIO_InitTypeDef initUserButton = {GPIO_PIN_0,
+																		GPIO_MODE_INPUT,
+																		GPIO_SPEED_FREQ_LOW,
+																		GPIO_PULLDOWN};
+	HAL_GPIO_Init(GPIOC, &initLEDS); // Initialize PC6 (red LED), PC7 ((blue LED), PC8 (orange LED), and PC9 (green LED)
+	HAL_GPIO_Init(GPIOA, &initUserButton);
+	HAL_GPIO_WritePin(GPIOC, GPIO_PIN_9, GPIO_PIN_SET); // Set green LED high
+	HAL_GPIO_WritePin(GPIOC, GPIO_PIN_8, GPIO_PIN_RESET); // Make sure orange led starts low																	
+	
+	// Enable/unmask interrupt generation on EXTI input line 0
+	EXTI->IMR |= (1 << 0);
+	// Configure the EXTI input line 0 to have a rising-edge-trigger
+	EXTI->RTSR |= (1 << 0);
+																		
+	// Configure the multiplexer to route PA0 to teh EXTI input line 0 // 3.2?
+	SYSCFG->EXTICR[0] &= ~(0x0000);
+	SYSCFG->EXTICR[0] |= 0x0000;
+																		
+	// Enable and set priority of EXTI interrupt
+	NVIC_EnableIRQ(EXTI0_1_IRQn); //Enable selected EXTI interrupt
+	//NVIC_SetPriority(EXTI0_1_IRQn,1); // Set priority of interrupt to 1 (high priority)			
+  NVIC_SetPriority(EXTI0_1_IRQn,3);		// Set it so its below new SysTick priority																
+  
+  while (1)
+  {
+    /* This LED indicates whether main loop is executing or if system is stuck in an interrupt.*/
+		HAL_Delay(600); // Delay 600 ms
+		HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_6); // Toggle red LED
+    
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
+
